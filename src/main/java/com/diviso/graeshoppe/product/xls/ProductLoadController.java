490761e1c50d18f695e6fc86f6070f757016ba72
sruthykc <sruthy.k.c@lxisoft.com>
2020-02-01 11:40:20 +0530

    log

--- src/main/java/com/diviso/graeshoppe/product/xls/ProductLoadController.java
+++ src/main/java/com/diviso/graeshoppe/product/xls/ProductLoadController.java
@@ -87,7 +87,7 @@ public class ProductLoadController {
 			tempCategory.setiDPcode(row.getCell(5).getStringCellValue());
 			tempCategory.setName(row.getCell(6).getStringCellValue());
 			
-			Log.info("_____________________________________________celllog"+row.getLastCellNum());
+			System.out.println("...........celllog.........."+row.getLastCellNum());
 			
 			if (row.getLastCellNum()==7)
 			{
